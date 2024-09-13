8153081e25b01be49c936099150483fadc3f49fc
25RML <ricardo.matamoros@unmsm.edu.pe>
2024-09-13 23:15:30 +0000

    que
    so
    😂🤣🤣😂🤣😂🤣😂🤣🤣😂😄😃😃🙂😐😕😕🙁🙁😧😰😰😰🥺

--- src/main/main.java
+++ src/main/main.java
@@ -12,5 +12,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("asdasdasd");
         System.out.println("HOLA SOY MIGUEL");
+        System.out.println("que");
     }
 }
