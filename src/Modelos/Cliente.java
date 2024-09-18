df8f8766e92d0814d4f5b316153bb83193c540c4
lightbluedev05 <mcrlightblue@gmail.com>
2024-09-18 18:19:12 -0500

    Primera PseudoEstructura

--- src/Modelos/Cliente.java
+++ src/Modelos/Cliente.java
@@ -1,15 +1,15 @@
 package Modelos;
 
 import java.util.Objects;
-
+//EL OBJETO CLIENTE COMO TAL
 public class Cliente {
-    String nombres;
-    String apellidos;
-    String telefono;
-    String dni;
-    String distrito;
-    String fecha_nacimiento;
-    int numero_viajes;
+    public String nombres;
+    public String apellidos;
+    public String telefono;
+    public String dni;
+    public String distrito;
+    public String fecha_nacimiento;
+    public int numero_viajes;
     
     public Cliente(String nombres, String apellidos, String telefono, String dni, String fecha_nacimiento, String distrito){
         this.nombres = nombres;
@@ -36,13 +36,4 @@ public class Cliente {
         }
     }
 
-    public void mostrar_perfil(){
-        System.out.println("Nombre: "+this.nombres);
-        System.out.println("Apellidos: "+this.apellidos);
-        System.out.println("Telefono: "+this.telefono);
-        System.out.println("DNI: "+this.dni);
-        System.out.println("Fecha de Nacimiento: "+this.fecha_nacimiento);
-        System.out.println("Distrito: "+this.distrito);
-        System.out.println("Numero de viajes: "+this.numero_viajes);
-    }
 }
