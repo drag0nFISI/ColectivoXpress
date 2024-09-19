ab3f946974cdc3b391d0c8e3fab56bd00eac7dbf
Mitsonn <mitsodmcyt@gmail.com>
2024-09-18 23:31:17 -0500

    arregla esta huevada

--- src/Vista/MenuPrincipal.java
+++ src/Vista/MenuPrincipal.java
@@ -1,10 +1,13 @@
 package Vista;
 import Modelos.Cliente;
+import Services.ClienteService;
 
 public class MenuPrincipal {
     //pseudo vista de menu principal
     public static void main(String[] args) {
-
+        ClienteService consola = new ClienteService();
+        Cliente cliente1 = consola.registro_cliente();
+        consola.mostrar_perfil(cliente1);
     }
 
 
